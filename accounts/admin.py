5e7be1b726986b9f5c05e16c5a8319d8a06d95ad
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-21 20:27:57 +0200

    Add accounts models to admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from accounts.models import Employee
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username",)
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("user__username", "full_name", "position")
