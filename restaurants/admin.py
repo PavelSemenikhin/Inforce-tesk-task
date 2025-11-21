5f8fe4faf4e0cf9339cd4b39e5b477d8a42b84ee
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-22 00:19:35 +0200

    Add restaurant model to admin panel

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from restaurants.models import Restaurant
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "type_of_food",
+        "created_at",
+    )
