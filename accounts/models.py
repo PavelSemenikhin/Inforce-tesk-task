c984c0b006afa69aa63a4f42930c8b6f01478aa4
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-21 18:34:19 +0200

    Add User and Employee models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+    class Meta:
+        ordering = ["-date_joined"]
+
+    def __str__(self):
+        return self.username
+
+
+class Employee(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="employee",
+    )
+    full_name = models.CharField(max_length=255)
+    position = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        db_table = "employees"
+        ordering = ["full_name"]
+
+    def __str__(self):
+        return self.full_name
