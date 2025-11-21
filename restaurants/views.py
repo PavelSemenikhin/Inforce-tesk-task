a7c5c2711019c79fdeaf1b7a693ee52e0991e0fc
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-22 00:19:50 +0200

    Add restaurant viewset

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from restaurants.models import Restaurant
+from restaurants.serializers import RestaurantSerializer
+
+
+class RestaurantViewSet(viewsets.ModelViewSet):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+
+    def get_permissions(self):
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [permissions.IsAdminUser()]
+        return [permissions.IsAuthenticated()]
