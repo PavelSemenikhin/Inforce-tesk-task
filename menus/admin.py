910873c50fa3ef8b6bec165c8e88fe1b3edaf9c6
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-22 00:23:12 +0200

    Add menu model to admin panel

--- menus/admin.py
+++ menus/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from menus.models import Menu
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("restaurant__name", "date", "dishes")
