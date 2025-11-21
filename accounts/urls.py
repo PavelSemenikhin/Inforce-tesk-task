444aa39234287efaacb3c589cf68db02d5070d59
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-21 19:19:58 +0200

    Add JWT and employees + register endpoints

---
+++ accounts/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+from accounts.views import EmployeeViewSet, RegisterViewSet
+
+app_name = "accounts"
+
+router = routers.DefaultRouter()
+router.register("employees", EmployeeViewSet, basename="employees")
+router.register("register", RegisterViewSet, basename="register")
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+]
