6da93d9af9fbfc45eb9f8c2c3dac3d341a18b134
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-22 00:24:31 +0200

    Add viewset for menu endpoints

--- menus/views.py
+++ menus/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from datetime import date
 
-# Create your views here.
+from rest_framework import viewsets, permissions
+
+from menus.models import Menu
+from menus.serializers import MenuReadSerializer, MenuCreateSerializer
+
+
+class MenuViewSet(viewsets.ModelViewSet):
+    """
+    Endpoints for  Menu with dishes like JSON field.
+    """
+
+    def get_queryset(self):
+        if self.action == "list":
+            return Menu.objects.filter(date=date.today())
+        return Menu.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return MenuReadSerializer
+        return MenuCreateSerializer
+
+    def get_permissions(self):
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [permissions.IsAdminUser()]
+        return [permissions.IsAuthenticated()]
