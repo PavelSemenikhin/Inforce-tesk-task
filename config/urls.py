f256698b7caf87dc45fb0e372c1b651f2a88519b
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-22 00:25:43 +0200

    Include urls.py of apps

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/accounts/", include("accounts.urls"), name="accounts"),
+    path(
+        "api/accounts/",
+        include("accounts.urls"),
+        name="accounts",
+    ),
+    path(
+        "api/restaurants/",
+        include("restaurants.urls"),
+        name="restaurants",
+    ),
+    path(
+        "api/menus/",
+        include("menus.urls"),
+        name="menus",
+    ),
     path(
         "api/docs/",
         SpectacularSwaggerView.as_view(url_name="schema"),
