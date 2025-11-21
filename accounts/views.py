e7e6de0b1bca1ce4f2569fd7d0461fdb13d3bcbe
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-21 18:36:30 +0200

    Add Register and Employee viewsets

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets, permissions, mixins
 
-# Create your views here.
+from accounts.models import Employee
+from accounts.serializers import (
+    RegisterSerializer,
+    EmployeeCreateSerializer,
+    EmployeeReadSerializer,
+)
+
+User = get_user_model()
+
+
+class RegisterViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve", "update", "partial_update", "destroy"]:
+            return [permissions.IsAdminUser()]
+        return [permissions.IsAuthenticated()]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return Employee.objects.all()
+        return Employee.objects.filter(user=user)
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return EmployeeCreateSerializer
+        return EmployeeReadSerializer
