32bc02b143f109c50940bb237d10de780a0341ad
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-22 00:20:50 +0200

    Register restaurants endpoints

---
+++ restaurants/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from restaurants.views import RestaurantViewSet
+
+app_name = "restaurants"
+
+router = DefaultRouter()
+router.register("restaurants", RestaurantViewSet, basename="restaurants")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
