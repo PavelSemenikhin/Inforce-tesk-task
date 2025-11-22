1ac5d40a865c1127f5e60a262dbcc188bef61400
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-22 02:06:48 +0200

    Add views for Votes with winner and results

--- votes/views.py
+++ votes/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+import datetime
 
-# Create your views here.
+from django.db.models.aggregates import Count
+from django.http import HttpRequest
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from menus.models import Menu
+from votes import services
+from votes.models import Vote
+from votes.serializers import VoteReadSerializer, VoteCreateSerializer
+
+
+class VoteViewSet(viewsets.ModelViewSet):
+    queryset = Vote.objects.all()
+
+    def get_permissions(self):
+        if self.action in ["create"]:
+            return [permissions.IsAuthenticated()]
+        return [permissions.IsAdminUser()]
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return VoteReadSerializer
+        return VoteCreateSerializer
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        menu = serializer.validated_data["menu"]
+        vote = services.create_vote_for_user(user, menu)
+        serializer.instance = vote
+
+
+class VoteResultApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    @staticmethod
+    def get(request: HttpRequest) -> Response:
+        results = services.get_vote_result()
+        return Response(results)
+
+
+class WinnerApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request: HttpRequest) -> Response:
+        winner = services.get_today_winner()
+
+        if winner is None:
+            return Response({"detail": "No votes today"}, status=200)
+        return Response(winner)
