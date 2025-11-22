e350d8b635b050310e8d7ec668df2ad04f4ece6f
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-22 02:21:56 +0200

    Add app_version middleware to settings

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,9 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+
+    "core.middlewares.version_middleware.AppVersionMiddleware",
+
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
