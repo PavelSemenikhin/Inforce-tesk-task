c0575641bb2f64d2df7360c7a2842b2834767cfe
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-22 02:03:12 +0200

    Add Vote model

--- votes/models.py
+++ votes/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models import UniqueConstraint
 
-# Create your models here.
+from accounts.models import Employee
+from menus.models import Menu
+
+
+class Vote(models.Model):
+    employee = models.ForeignKey(
+        Employee,
+        on_delete=models.CASCADE,
+        related_name="votes",
+    )
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name="votes")
+    date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=["employee", "date"],
+                name="unique_employee_date",
+            ),
+        ]
+        db_table = "votes"
+        ordering = ["-date"]
+
+    def __str__(self):
+        return f"{self.employee.full_name}-{self.menu.restaurant.name}-{self.date}."
